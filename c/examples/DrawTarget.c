f4841fbad2f16d9911eb699020a6e70b219eb2f8
Markus Heckner <markus.heckner@oth-regensburg.de>
2016-11-29 16:16:39 +0100

    finished drawTarget example

--- c/examples/DrawTarget.c
+++ c/examples/DrawTarget.c
@@ -15,23 +15,47 @@
 /* Constants */
 
 const int WIDTH = 800;
-const int HEIGHT = 300;
+const int HEIGHT = 800;
 
-const int BRICK_WIDTH = 30;
-const int BRICK_HEIGHT = 12;
-const int BRICKS_IN_BASE = 14;
-const string BRICK_COLOR = "ORANGE";
+const int START_DIAMETER = 30;
+const int NUM_TARGETS = 15;
+const string EVEN_COLOR = "WHITE";
+const string UNEVEN_COLOR = "RED";
 
 GWindow gw;
 
 /* Prototypes */
 
 void setupCanvas();
+void drawTarget();
+void drawOneRing(int ringNum, string ringColor);
 
 int main() {
 	setupCanvas();
-	/* TODO */
+	drawTarget();
 	return 0;
+}
+
+void drawTarget() {
+	string ringColor;
+	int ringNum;
+	for (ringNum = NUM_TARGETS; ringNum >= 1; ringNum--) {
+		if (ringNum % 2 == 0) {
+			ringColor = EVEN_COLOR;
+		} else {
+			ringColor = UNEVEN_COLOR;
+		}
+		drawOneRing(ringNum, ringColor);
+	}
+}
+
+void drawOneRing(int ringNum, string ringColor) {
+	int diameter = ringNum * START_DIAMETER;
+	int offset = diameter / 2;
+	GOval ring = newGOval(WIDTH / 2 - offset, HEIGHT / 2 - offset, diameter, diameter);
+	setFilled(ring, true);
+	setFillColor(ring, ringColor);
+	draw(gw, ring);
 }
 
 void setupCanvas() {
